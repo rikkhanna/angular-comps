c38ec740ec7b98464e527c1892f1b91b7775c60c
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-06-22 00:56:31 +0530

    reusable table comp with some data and styling

--- src/app/collections/collection-home/collection-home.component.ts
+++ src/app/collections/collection-home/collection-home.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collection-home.component.css']
 })
 export class CollectionHomeComponent implements OnInit {
-
+  
+  data = [
+    { name: 'James', age: 30, job: 'Designer' },
+    { name: 'Jill', age: 28, job: 'Engineer' },
+    { name: 'Elyse', age: 32, job: 'Engineer' }
+  ];
+  headers = [
+    {key:'name',label: 'Name'},
+    { key:'age', label: 'Age'},
+    { key:'job', label: 'Job'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
